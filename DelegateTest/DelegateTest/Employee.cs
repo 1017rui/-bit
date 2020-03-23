0d2db0a35eefd4c8eb4decda1f2a66201bdc9537
1017rui <541709413@qq.com>
2020-03-23 23:20:25 +0800

    用c#实现“经理发现员工玩游戏扣工资”的过程， 第一次：写死了只有经理才会抓员工玩游戏，程序过于死板。 第二次：用事件和委托机制来处理，创建Employee,Manager,使得程序灵活。

---
+++ DelegateTest/DelegateTest/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTest
+{
+    class Employee
+    {
+        public event PlayGameHandler play;//事件委托如何方便的实现程序的绑定。
+        private int m_salary;
+
+        public int Salary
+        {
+            get { return m_salary; }
+            set { m_salary = value; }
+        }
+        public Employee()
+        {
+            Console.WriteLine("创建员工对象！");  
+           m_salary=3000;
+        }
+        public void playGame()
+        {
+            Console.WriteLine("游戏真好玩！");
+            EventArgs e = new EventArgs();
+            onPlayGame(e);
+        }
+        public virtual void onPlayGame(EventArgs e)
+        {
+            if (play != null)
+                play(this, e);
+            else
+                Console.WriteLine("玩游戏处理时间未定义");
+        }
+    }
+}
---
+++ DelegateTest/DelegateTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTest
+{
+    //定义委托
+    public delegate void PlayGameHandler(object sender,EventArgs e);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("场景开始！");
+            //Manager_ori zhang = new Manager_ori();
+            //Employee_ori wang =new Employee_ori(zhang);
+            //Console.WriteLine("员工初始薪水为：" + wang.Salary);
+            //wang.playGame();
+            //System.Threading.Thread.Sleep(2000);
+            //Console.WriteLine("员工现在薪水为：" + wang.Salary);
+
+
+
+            Employee wang = new Employee();
+            Manager zhang = new Manager();
+            Console .WriteLine("员工初始薪水为："+wang.Salary);
+            wang.play += new PlayGameHandler(zhang.deductSalary);
+            System.Threading.Thread.Sleep(2000);
+            wang.playGame();
+            Console.WriteLine("员工现在薪水为：" + wang.Salary);
+
+            Console.WriteLine("场景结束！");
+            Console.ReadLine();
+
+        }
+    }
+}
