555fa6dba4c741af132eaaf2ee75d8ec572031c6
1017rui <541709413@qq.com>
2020-04-09 23:19:51 +0800

    java基础相关

---
+++ 20200406/HelloWorld.java
@@ -0,0 +1,19 @@
+
+//如果一个类时public的，那么这个类的类名 一定要和文件名相同，并且一个javs文件当中只能有一个公共的类。
+//class A{
+//}
+public class HelloWorld {
+	public static void main(String[] args) {
+		int a;
+		System.out.println(a);
+		//int max=Integer.MIN_VALUE+1;
+		//System.out.println(max);
+		//int a=10;
+		//System.out.println(Integer.MAX_VALUE);
+		//System.out.println(Integer.MIN_VALUE);
+		//System.out.println(a);//打印且换行
+		//System.out.print(a);//打印不换行
+		//System.out.printf("%d\n",a);//以格式化的形式进行输出内容
+		
+	}
+}
---
+++ 20200408/TextDemo.java
@@ -0,0 +1,111 @@
+public class TextDemo{
+	public static void main(String[] args){
+		// long a=10L;
+		// int b=(int)a;
+	
+		// System.out.println(b);
+	//	int num=10;
+		//String str1=num+"";
+		//String str2=String.valueOf(num);
+		// double x=2.0;
+		// int y=4;
+		// x/=++y;
+		// System.out.println(x);
+		// System.out.println("\\\"hello\\\"");
+		int a=3;
+		byte b=a;
+	}
+}
+		//byte b=10;
+		// byte d=20;
+		// byte f=(byte)(b+d);
+		// System.out.println(f);
+	// }
+// }
+		// // //字符串数据类型：String
+		// String str="\"hello\"";
+		// System.out.println(str);
+	// }
+// }
+		// System.out.println(Double.MAX_VALUE);
+		// System.out.println(Double.MIN_VALUE);
+	// }
+// }
+		//布尔类型
+		//在java当中 布尔类型 没有明确的大小
+		//在java当中  布尔类型 只有两个值 true flase
+		// boolean flg=false;
+		// System.out.println(flg);
+	// }
+// }
+		//短整型：short 2   -32768---32767
+		 // short sh=12;
+		 // System.out.println(Short.MAX_VALUE);//32767
+		 // System.out.println(Byte.MIN_VALUE);//-128
+// }
+// }
+		//字节：byte 1 个字节 -128--127
+		// byte b=12;
+		// byte c=21;
+		// System.out.println(b+""+c);
+		// System.out.println(Byte.MAX_VALUE);
+		// System.out.println(Byte.MIN_VALUE);
+		// System.out.println(Byte.MAX_VALUE+1);//默认以整型进行打印
+		// byte d=130;//127
+		
+	// }
+// }
+		//byte d=Byte.MAX_VALUE+1;
+		//System.out.println(d);
+	// public static void main5(String[] args){
+		//char 两个字节 0-65535
+		//unicode-->它包含了很多，中文，各种符号，变得更大了，表示的范围更广了。
+		// char ch='a';
+		// System.out.println(ch);
+		// char ch1='高';
+		// System.out.println(ch1);
+		// char ch2=97;
+		// System.out.println(ch2);
+		// public static void main4(String[] args){
+		
+		// float f=12.3f;
+		// System.out.println(f);
+	 
+	 //public static void main3(String[] args) { 
+		// double d=12.5;//默认双精度类型
+		// System.out.println(d);
+		// System.out.println(Double.MAX_VALUE);
+		// System.out.println(Double.MIN_VALUE);
+		// int a=1;
+		// int b=2;
+		// System.out.println(a/b);
+		// System.out.println((double)a/b);
+		// double num=1.1;
+		// System.out.println(num*num);
+		
+		
+	//}
+//}
+	// public static void main(String[] args) {
+	// for(int i=0;i<args.length;i++)
+	// {
+		// System.out.println(args[i]);
+	// }
+	// System.out.println("kaixin");
+	// }
+// }
+	//讲解长整型
+	//public static void main1(String[] args) {
+		//long a=10L;
+		
+		//System.out.println("a的值是："+a);
+		//System.out.println("最大值："+Long.MAX_VALUE);//拼接
+		//System.out.println("最大值："+Long.MIN_VALUE);
+		
+		//int b=10;
+		//int c=20;
+		//System.out.println(b+c);
+		//System.out.println("hhh"+b+c);//注意：任何类型的数据和字符串进行拼接，结果都是字符串
+		//System.out.println("Hello");
+	//}
+//}
